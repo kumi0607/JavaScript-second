02979715fe03b82ffd47228b003738cec3a826f6
kumi0607 <d935.yksmdatti@gmail.com>
2023-03-17 17:32:03 +0900

    firstcommit js to 7

---
+++ js/if-switch.js
@@ -0,0 +1,32 @@
+// 変数numに0~4までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 5);
+
+console.log(num);
+
+if (num === 4) {
+    console.log('大当たりです');
+} else if (num === 3) {
+    console.log('当たりです');
+} else {
+    console.log('はずれです');
+}
+
+// 変数を書き換えたいときはletの宣言をせず変数の中身の身を変更する
+num = Math.floor(Math.random() * 5);
+
+console.log(num);
+
+switch (num) {
+    case 2:
+        console.log('小吉です');
+        break;
+    case 3:
+        console.log('中吉です');
+        break;
+    case 4:
+        console.log('大吉です');
+        break;
+    default:
+        console.log('末吉です');
+        break;
+}
