3f53ce7067325fe2298e1b8fec437d803111f9f6
kumi0607 <d935.yksmdatti@gmail.com>
2023-03-21 21:57:36 +0900

    dom.js

---
+++ js/dom.js
@@ -0,0 +1,16 @@
+// head要素を取得し中身を出力する
+console.log(document.head);
+// body要素を取得し中身を出力する
+console.log(document.body);
+
+// windowオブジェクトの中身を出力する
+console.log(window);
+
+// html要素をidで取得し、中身を出力する
+console.log(document.getElementById('first-list'));
+console.log(document.getElementsByClassName('list'));
+
+console.log(document.getElementsByClassName('heading'));
+console.log(document.getElementById('second-list'));
+
+
