9a95228416fce25d5367577172e3bee8a97c34ad
kumi0607 <d935.yksmdatti@gmail.com>
2023-03-21 15:00:07 +0900

    function.js

---
+++ js/function.js
@@ -0,0 +1,26 @@
+function sayGoodMorning () {
+    console.log('おはようございます');
+    console.log('昨日はよくねむれましたか');
+    console.log('今日も一日頑張りましょう');
+}
+
+function sayGoodEvening () {
+    console.log('こんばんわ');
+    console.log('今日も一日お疲れさまでした');
+}
+
+sayGoodMorning ();
+
+sayGoodEvening ();
+
+function calculateTotal (price) {
+    console.log(price + 500 + '円')
+}
+calculateTotal (1200);
+
+function double (num) {
+    return num*2;
+}
+console.log(double(30));
+
+
